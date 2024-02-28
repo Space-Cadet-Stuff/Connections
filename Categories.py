16b9f24449c2b0941e8beb1815627d4f3ad888a4
Fish <jshfshr@outlook.com>
2024-02-28 20:32:12 +1100

    first commit

---
+++ Categories.py
@@ -0,0 +1,22 @@
+categories = {
+    'Metalcore Bands': ['Architects','Currents','Volumes','Imminence'],
+    'Sith Lords (Darth______)': ['Sion','Tenebrae','Kreia','Bane'],
+    'Rockets':['Titan','Ariane','Antares','Saturn'],
+    'STARSET Albums': ['Transmissions','Vessels','Horizons','Divisions'],
+    'Characters in Greek Mythology': ['Prometheus','Hercules','Ares','Alecto'],
+    'Creature in a Horror movie name, Silence of the LAMBS, The Human CENTIPEDE, ALIEN, BIRD Box': ['Lambs','Centipede','Alien','Bird'],
+    'Incects': ['Grasshopper','Bee','Wasp','Spitfire'],
+    'US Aircraft': ['Thunderbolt','Blackbird','Lightning','Raider'],
+    'Bird Species': ['Wren','Sparrow','Owl','Emu'],
+    'Aliens': ['Calvin','Yautja','Hutt','Whitespike'],
+    'Australian Icons': ['Kangaroo','Boomerang','Aukubra','Uluru'],
+    'Computer Games': ['Portal','Rust','Doom','Stray'],
+    'Polyatomic Anions': ['Sulphate','Oxide','Fluoride','Chlorine'],
+    'Extraterrestrial Mountain Ranges, ______ Montes': ['Caloris','Mithrim','Tenzing','Taurus'],
+    'Probes to Saturn': ['Voyager','Cassini','Pioneer','Huygens'],
+    'Cheese': ['Swiss','Cottage','Cream','Blue'],
+    'Musician Nicknames': ['Sting','Slash','Flea','Bonzo'],
+    'Fish Species': ["Bass","Pike","Shad","Snook"],
+    'Alice In Chains Songs': ["Would","Nutshell","Hollow","Rooster"],
+    'Haggis Ingredients': ["Pluck","Oatmeal","Onions","Suet"]
+}
---
+++ DCategories.py
@@ -0,0 +1,6 @@
+categories = {
+    'Test1': ["A","B","C","D"],
+    'Test2': ["E","F","G","H"],
+    'Test3': ["I","J","K","L"],
+    'Test4': ["M","N","O","P"]
+}
